220d60a81f5e5e853c32e274ebb92f15f37b29f9
Poprádi Gabriella <h268517@stud.u-szeged.hu>
2025-03-16 20:13:02 +0100

    .

--- server.js
+++ server.js
@@ -98,11 +98,11 @@ app.post('/pdf-to-image', upload.single('file'), async (req, res) => {
 
 app.use(express.static(path.join(__dirname, './browser')));
 app.get('/kezdooldal', (req, res) => {
-  res.sendFile(path.join(__dirname, '/browser/index.html'));
+  res.sendFile(path.join(__dirname, './browser/index.html'));
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/browser/index.html'));
+  res.sendFile(path.join(__dirname, './browser/index.html'));
 })
 
 // Express szerver indítása
